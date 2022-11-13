17ed1294e4de796ce59eac659e23a10f7a5cecd8
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-13 16:13:36 +0800

    shino_hecate

--- sharding5_jdbc01/src/test/java/com/atguigu/Sharding5Jdbc01ApplicationTests.java
+++ sharding5_jdbc01/src/test/java/com/atguigu/Sharding5Jdbc01ApplicationTests.java
@@ -6,11 +6,31 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class Sharding5Jdbc01ApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
+
+
+
+    @Test
+    void testInsertAndSelect(){
+
+        User user = new User();
+        user.setUname("优纪");
+        userMapper.insert(user);
+
+        List<User> users = userMapper.selectList(null);
+        System.out.println("====");
+        System.out.println("====");
+        System.out.println("====");
+        users.forEach(System.out::println);
+
+
+    }
 
     @Test
     void contextLoads() {
